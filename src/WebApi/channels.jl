97b6ec673ac6fd6d3f140ab959019843fe096177
Erik Edin <erikedin.name@gmail.com>
2016-03-27 14:29:30 +0200

    implement channels info and archive

--- src/WebApi/channels.jl
+++ src/WebApi/channels.jl
@@ -1,6 +1,14 @@
 export
     ChannelsList,
-    ChannelsListResponse
+    ChannelsListResponse,
+    ChannelsInfo,
+    ChannelsInfoResponse,
+    ChannelsArchive,
+    ChannelsArchiveResponse
+
+##
+## channels.list
+##
 
 immutable ChannelsList
     token::Token
@@ -13,3 +21,37 @@ end
 
 getresponsetype(::Type{ChannelsList}) = ChannelsListResponse
 method_name(::Type{ChannelsList}) = "channels.list"
+
+##
+## channels.info
+##
+
+immutable ChannelsInfo
+    token::Token
+    channel::ChannelId
+end
+
+immutable ChannelsInfoResponse
+    channel::Channel
+end
+
+getresponsetype(::Type{ChannelsInfo}) = ChannelsInfoResponse
+method_name(::Type{ChannelsInfo}) = "channels.info"
+
+##
+## channels.archive
+##
+
+immutable ChannelsArchive
+    token::Token
+    channel::ChannelId
+end
+
+# Note: ChannelsArchiveResponse is intentionally empty, because we get no more information back than
+# the status.
+immutable ChannelsArchiveResponse
+end
+
+getresponsetype(::Type{ChannelsArchive}) = ChannelsArchiveResponse
+method_name(::Type{ChannelsArchive}) = "channels.archive"
+
