1b278a81fc07e4a36f05f8b2f856392be0740f1c
Erik Edin <erikedin.name@gmail.com>
2016-04-25 06:40:00 +0200

    finish send_event function

--- src/RTM/rtm.jl
+++ src/RTM/rtm.jl
@@ -1,6 +1,9 @@
 using WebSocketClient
+import JSON
 
 abstract RTMEvent
+
+serialize(event::RTMEvent) = error("serialize not implemented for $(event)")
 
 type RTMClient
     client::AbstractWSClient
@@ -12,5 +15,13 @@ end
 function send_event(c::RTMClient, event::RTMEvent)
     this_id = c.next_id
     c.next_id += 1
+
+    dict = serialize(event)
+    dict["id"] = this_id
+    text = utf8(JSON.json(dict))
+    send_text(c.client, text)
+
     this_id
 end
+
+close(c::RTMClient) = stop(c.client)
--- test/rtm_test.jl
+++ test/rtm_test.jl
@@ -1,10 +1,40 @@
 using WebSocketClient
+import JSON
+import Base.==
 
-immutable FakeEvent <: DandelionSlack.RTMEvent end
+immutable FakeEvent <: DandelionSlack.RTMEvent
+    value::UTF8String
+
+    FakeEvent() = new("")
+    FakeEvent(a::ASCIIString) = new(utf8(a))
+end
+
+==(a::FakeEvent, b::FakeEvent) = a.value == b.value
+
+DandelionSlack.serialize(event::FakeEvent) = Dict{AbstractString, Any}("value" => event.value)
+
+test_event_1 = FakeEvent("bar")
+test_event_2 = FakeEvent("baz")
 
 type MockWSClient <: AbstractWSClient
+    sent::Vector{Dict{Any, Any}}
+    closed_called::Int
 
+    MockWSClient() = new([], 0)
 end
+
+WebSocketClient.stop(c::MockWSClient)                     = c.closed_called += 1
+WebSocketClient.send_text(c::MockWSClient, s::UTF8String) = push!(c.sent, JSON.parse(s))
+
+function expect_event(c::MockWSClient, id::Int64, value::UTF8String)
+    @fact c.sent --> x -> !isempty(x)
+
+    parsed = shift!(c.sent)
+    @fact parsed["id"] --> id
+    @fact parsed["value"] --> value
+end
+
+expect_close(c::MockWSClient; no_of_closes::Int=1) = @fact c.closed_called --> no_of_closes
 
 facts("RTM events") do
     context("Increasing message id") do
@@ -16,5 +46,23 @@ facts("RTM events") do
         message_id_3 = DandelionSlack.send_event(rtm, FakeEvent())
 
         @fact message_id_1 < message_id_2 < message_id_3 --> true
+    end
+
+    context("Sending events") do
+        ws_client = MockWSClient()
+        rtm = DandelionSlack.RTMClient(ws_client)
+
+        id_1 = DandelionSlack.send_event(rtm, test_event_1)
+        id_2 = DandelionSlack.send_event(rtm, test_event_2)
+
+        expect_event(ws_client, id_1, test_event_1.value)
+        expect_event(ws_client, id_2, test_event_2.value)
+    end
+
+    context("Send close on user request") do
+        ws_client = MockWSClient()
+        rtm = DandelionSlack.RTMClient(ws_client)
+        DandelionSlack.close(rtm)
+        expect_close(ws_client)
     end
 end
