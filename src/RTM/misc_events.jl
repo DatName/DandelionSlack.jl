964c49b6b7e3129d29c991d88a07f87237e921c6
Erik Edin <erikedin.name@gmail.com>
2016-05-12 21:31:29 +0200

    add common RTM events

--- src/RTM/misc_events.jl
+++ src/RTM/misc_events.jl
@@ -15,3 +15,17 @@ end
 @slackevent(ErrorEvent, "error", begin
         error::RTMError
     end)
+
+@slackevent(PresenceChangeEvent, "presence_change", begin
+        presence::UTF8String
+        user::UserId
+    end)
+
+@slackevent(UserTypingEvent, "user_typing", begin
+        channel::ChannelId
+        user::UserId
+    end)
+
+@slackevent(ReconnectUrlEvent, "reconnect_url", begin
+        url::UTF8String
+    end)
